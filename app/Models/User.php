2209a02602d43f7021480776933835e21f37abfe
Vladimir Patrikeev <vpatrikeev@wall.black>
2022-01-10 16:13:19 +0300

    Fix codestyle

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,9 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
